9d1d1182b78ba35170bc96843a87e9eae9931ef0
张忠旺 <727271755@qq.com>
2018-10-09 11:40:54 +0800

    add docker

---
+++ docker/ssl/sign.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+openssl x509 -req -sha256 -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -days 3650 -out server.crt -extensions v3_req -extfile server.cnf
+
+cat server.crt server.key > server.pem
