fdc152f092a336bbe3abc5867810c44de565a6c9
run <727271755@qq.com>
2018-11-20 23:01:46 +0800

    auth

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ Route::get('/', function () {
 });
 
 Route::get('/world', ['uses'=>'\App\Http\Controllers\Controller@world']);
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
